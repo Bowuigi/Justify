4601fd5e516fdcf4ed611eb42e396cc594a53258
Bowuigi <bowuigi@vern.cc>
2026-01-21 04:09:25 -0300

    fix(format): Add a max_results field to Query and some more examples

--- formats/query.ts
+++ formats/query.ts
@@ -4,6 +4,7 @@ import { SIdentifier, SIdentifierTeXMap, STerm } from './common';
 export const SQuery = z.strictObject({
   variables: SIdentifierTeXMap,
   literals: SIdentifierTeXMap,
+  max_results: z.int().positive(),
   relation: SIdentifier,
   args: z.array(STerm),
 });
